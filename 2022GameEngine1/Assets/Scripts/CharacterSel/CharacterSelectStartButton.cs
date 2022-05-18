35ef3be1a9dff5c107f50b5484dd94e6a61de259
Hojin Kim <hojiny9923@gmail.com>
2022-05-19 07:07:37 +0900

    캐릭터 선택창

---
+++ 2022GameEngine1/Assets/Scripts/CharacterSel/CharacterSelectStartButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelectStartButton : MonoBehaviour
+{
+    public void OnCLick()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
