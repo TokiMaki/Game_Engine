abf5c849fc82ec4b0087f0e16beebe36dcbdf8b2
2017182026 / youn jang hyurk <34494633+uhreungdu@users.noreply.github.com>
2022-05-19 14:07:29 +0900

    ppt제작 및 게임매니저 작성

---
+++ 2022GameEngine1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager GetInstance()
+    {
+        if(instance == null)
+        {
+            instance = FindObjectOfType<GameManager>();
+            if(instance == null)
+            {
+                GameObject container = new GameObject("GameManager");
+                instance = container.AddComponent<GameManager>();
+            }
+        }
+        return instance;
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
