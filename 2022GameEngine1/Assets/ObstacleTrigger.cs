2643e171e92400ecc9e4112fcbb035922c553794
2017182026 / youn jang hyurk <34494633+uhreungdu@users.noreply.github.com>
2022-06-10 12:23:48 +0900

    스카이박스 적용 및 바닥 텍스처 변경, 이펙트 넣을 준비

--- 2022GameEngine1/Assets/ObstacleTrigger.cs
+++ 2022GameEngine1/Assets/ObstacleTrigger.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class ObstacleTrigger : MonoBehaviour
 {
+    public GameObject effect_con;
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Player")
