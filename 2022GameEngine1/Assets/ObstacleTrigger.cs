0a011fd11f33a29811985f4d720617d2df83207f
2017182026 / youn jang hyurk <34494633+uhreungdu@users.noreply.github.com>
2022-06-11 18:42:51 +0900

    상자 터지면 이펙트 나옴

--- 2022GameEngine1/Assets/ObstacleTrigger.cs
+++ 2022GameEngine1/Assets/ObstacleTrigger.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 public class ObstacleTrigger : MonoBehaviour
 {
     public GameObject effect_con;
+    public GameObject effect_obj;
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Player")
         {
+            effect_obj = Instantiate(effect_con);
+            effect_obj.transform.SetParent(gameObject.transform);
+            effect_obj.transform.position = gameObject.transform.position;
+            effect_obj.transform.parent = null;
             Destroy(gameObject);
         }
     }
