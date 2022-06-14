0e53069de49d0f326e01e7b6188a6666288be6ce
Hojin Kim <hojiny9923@gmail.com>
2022-06-15 06:52:26 +0900

    컨텐츠 추가
    이제 더 안할거

--- 2022GameEngine1/Assets/Scripts/GameManager.cs
+++ 2022GameEngine1/Assets/Scripts/GameManager.cs
@@ -59,6 +59,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        print(nowMeasure);
         if (playerstates.health <= -9000)
         {
             playerstates.Die();
