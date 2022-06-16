8cb86d0c77a2a07579c30cde3c6950895c0764a7
Hojin Kim <hojiny9923@gmail.com>
2022-06-16 14:35:43 +0900

    점수 기록용 스크립트
    Hojin

---
+++ 2022GameEngine1/Assets/Scripts/Result/RankingBoard.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class RankingBoard : MonoBehaviour
+{
+    private int _record = 0;
+    private static string _dataPath;
+
+    // 점수 불러오기
+    private void LoadRecord(string path)
+    {
+        FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read);
+        StreamReader sr = new StreamReader(fs);
+        try
+        {
+            _record = int.Parse(sr.ReadLine());
+        }
+        catch
+        {
+            
+        }
+
+        sr.Close();
+        Debug.Log(_record);
+    }
+
+    // 점수 파일에 기록
+    private void WriteRecord(string path, int score)
+    {
+        FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+        StreamWriter sw = new StreamWriter(fs);
+        sw.Write(score);
+    }
+
+    private void Start()
+    {
+        _dataPath = Application.persistentDataPath + "/" + "test";
+        LoadRecord(_dataPath);
+    }
+
+    // 불러온 점수 불러오기
+    public int GetScore()
+    {
+        return _record;
+    }
+}
